1be85b8529c1843d11df0fb31a8af96e772342be
Mina Atef <mina2000atef@gmail.com>
2023-05-21 17:35:58 +0300

    until services

---
+++ main.js
@@ -0,0 +1,20 @@
+// let icon = document.querySelector(".icon");
+// let drop = document.querySelector(".drop");
+// let container = document.querySelector(".container")
+// let switchIt = false;
+// icon.addEventListener("click", (e)=>{
+//     drop.style.display = "block";
+//     console.log(e.target.name);
+//     switchIt = true
+
+// })
+
+// container.addEventListener("click", e =>{
+//     if(switchIt){
+//         drop.style.display = "block";
+//         switchIt = false
+//     }else{
+//         drop.style.display = "none";
+
+//     }
+// })
